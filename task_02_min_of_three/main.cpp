2e72e690f081d1d4172081babc3aac7f926a6351
VanessTuracova <vanessa.turacova@gmail.com>
2025-11-03 17:14:46 +0100

    spravila som domacu ulohu
    spravila som domacu ulohu

--- task_02_min_of_three/main.cpp
+++ task_02_min_of_three/main.cpp
@@ -5,6 +5,11 @@ int main() {
     std::cin.tie(nullptr);
     long long a,b,c;
     if (!(std::cin >> a >> b >> c)) return 0;
-    // TODO: print the minimum of a, b, c
+    
+    long long minimum=a;
+    if (b<minimum) minimum=b;
+    if (c<minimum) minimum=c;
+
+    std::cout<<minimum;
     return 0;
 }
