a93f36756656fc0f88acf5316677a902962617c1
Jan Juritka <vanessa.turacova@gmail.com>
2025-11-19 14:01:59 +0100

    dokoncim si este 8
    dokoncim si este 8

--- task_08_frequency_table/main.cpp
+++ task_08_frequency_table/main.cpp
@@ -5,25 +5,16 @@
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
+    int n[101] = {};
 
-    int numbers[1000]; 
+    n[1] += 1;
+    n[2] += 1;
+    n[2] += 1;
+    n[100] += 1;
+    n[10] += 1;
 
-    for (int i = 0; i < n; i++) {
-        cin >> numbers[i];
-    }
-    for (int value = 0; value <= 100; value++) {
-        int count = 0;
-
-     for (int i = 0; i < n; i++) {
-        if (numbers[i] == value) {
-        count++;
-         }
-        }
-        if (count > 0) {
-            cout << value << " " << count << endl;
-        }
+    for (int pocet : n) {
+        cout << pocet << " ";
     }
     return 0;
 }
